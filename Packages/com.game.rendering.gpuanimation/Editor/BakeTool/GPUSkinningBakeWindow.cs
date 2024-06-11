ccbf9f7dca183f3a85a1cb330e93f18988c65db9
DESKTOP-77O6SI9\Administrator <410830616@qq.com>
2024-06-11 19:16:10 +0800

    init

---
+++ Packages/com.game.rendering.gpuanimation/Editor/BakeTool/GPUSkinningBakeWindow.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.GPUAnimation.Editor
+{
+    public class GPUSkinningBakeWindow : MonoBehaviour
+    {
+
+
+    }
+}
